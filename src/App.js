1445a393520cd151f4966043f53d95db75293219
Javier Neoh <jneoh001@e.ntu.edu.sg>
2023-03-03 14:47:20 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, { useState } from 'react'
+import Title from './components/Title'
+import Modal from './components/Modal'
+import EventList from './components/EventList'
+
+// if a function starts with the use Keywrod, it is a react hook.
 
 function App() {
+  const [showModal, setShowModals] = useState(false)
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "marios birthday", id: 1},
+    {title: "bowsers live stream", id: 2},
+    {title: "race on moo moo farm", id: 3},
+  ])
+  
+  const handleClick = (id) => {
+    setEvents( (prevEvents) => {
+      return prevEvents.filter( (event)=> {
+        return id !== event.id
+      })
+    } ) 
+    console.log(id)
+  }
+
+  const handleClose = () => {
+    setShowModals(false)
+  }
+  const subtitle = "All the latest events in Marioland"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      <Title title="Events in Your Area" subtitle = {subtitle} />
+
+
+      {showEvents &&
+        (<div>
+          <button onClick={() => setShowEvents(false) }>Hide events</button>
+        </div>
+      )}
+
+      {!showEvents && (<div>
+        <button onClick={() => setShowEvents(true)}> Show Events </button>
+      </div>
+      )}
+
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+
+      <div>
+        <button onClick = {()=> setShowModals(true)}>Show Modal</button>
+      </div>
+
+      {showModal && <Modal handleClose = {handleClose}>
+        <h2>10% Discount Code</h2>
+        <p>Use the code NINJA10 at the checkout</p>
+      </Modal>}
+
+
+
     </div>
   );
 }
